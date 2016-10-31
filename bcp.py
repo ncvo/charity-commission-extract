b25ac514001ccb2f0f53bfc85086efcd2dd8a612
David Kane <David.Kane@ncvo.org.uk>
2016-10-31 10:43:34 +0000

    Add BCP streamer

--- bcp.py
+++ bcp.py
@@ -3,52 +3,87 @@ import sys
 import csv
 
 def convert(bcpdata, lineterminator='*@@*', delimiter='@**@', quote='"', newdelimiter=',', escapechar='\\', newline='\n'):
+    """
+    returns data from a string of BCP data. Default is to present as CSV data.
+    """
     bcpdata = bcpdata.replace(escapechar, escapechar + escapechar)
     bcpdata = bcpdata.replace(quote, escapechar + quote)
     bcpdata = bcpdata.replace(delimiter, quote + newdelimiter + quote)
     bcpdata = bcpdata.replace(lineterminator, quote + newline + quote)
     bcpdata = quote + bcpdata + quote
     return bcpdata
+
+def stream(file, lineterminator='*@@*', delimiter='@**@', encoding='utf-8'):
+    """
+    Returns a generator for parsing a BCP file. The input is a file object, and 
+    the output is a list for each row in the data.
     
-def to_file(bcpdata, csvfilename="", col_headers=None):
-    if csvfilename=="":
-        csvfilename = 'converted.csv'
-        
-    # have to check system version annoyingly
-    if sys.version_info >= (3,0):
+    Usage:
     
-        # python3 csv writer needs strings
-        with open(csvfilename, 'w', encoding='utf-8') as csvfile:
-            if(col_headers):
-                for c in col_headers:
-                    c = c
-                writer = csv.writer(csvfile)
-                writer.writerow(col_headers)
-            csvfile.write(bcpdata)
-        
-    else:
+    with open("bcp_file.bcp", 'rb') as bcpfile:
+        for bcpfields in bcp_stream(bcpfile):
+            print(bcpfields)
     
-        # python2 csv writer needs bytes
-        with open(csvfilename, 'wb') as csvfile:
-            if(col_headers):
-                for c in col_headers:
-                    c = c.encode('utf-8')
-                writer = csv.writer(csvfile)
-                writer.writerow(col_headers)
-            csvfile.write(bcpdata.encode('utf-8'))
+    More pythonic, but slower than using the `convert()` function above and then 
+    parsing the CSV file, as the python CSV utilities are written in pure C.
+    """
         
-    return csvfilename
+    buffer = ''
+    fields = []
+    
+    while True:
+        byte = file.read(1)
+        try:
+            byte = byte.decode(encoding)
+        except AttributeError:
+            pass
+        
+        buffer += byte
+        
+        if not byte:
+            break
+        
+        # check if we've seen a field delimiter
+        if( buffer[-len(delimiter):] == delimiter):
+            fields.append( buffer[:-len(delimiter)] )
+            buffer = ''
+        
+        # check if we've seen a line delimiter
+        if( buffer[-len(lineterminator):] == lineterminator):
+            fields.append( buffer[:-len(lineterminator)] )
+            buffer = ''
+            yield fields
+            fields = []
+    
+    yield fields
 
 def main():
+
+    # get arguments
     bcp_filename = sys.argv[1]
     try:
         csv_filename = sys.argv[2]
     except IndexError:
         csv_filename = bcp_filename.replace('.bcp', '.csv')
-    with open(bcp_filename, 'rb') as bcpfile:
-        bcpdata = bcpfile.read()
-        bcpdata = convert(bcpdata)
-        to_file(bcpdata, csv_filename=csv_filename)
+        
+    # have to check system version annoyingly
+    # for python 3 >
+    if sys.version_info >= (3,0):
+        
+        with open(bcp_filename, 'r') as bcpfile:
+            with open(csv_filename, 'w', newline='') as csvfile:
+                writer = csv.writer(csvfile)
+                for bcpfields in stream(bcpfile):
+                    writer.writerow(bcpfields)
+    
+    # for python 2
+    else:
+        
+        with open(bcp_filename, 'rb') as bcpfile:
+            with open(csv_filename, 'wb') as csvfile:
+                writer = csv.writer(csvfile)
+                for bcpfields in stream(bcpfile):
+                    writer.writerow(bcpfields)
 
 if __name__ == '__main__':
     main()
--- import.py
+++ import.py
@@ -3,6 +3,7 @@ from __future__ import print_function
 import bcp
 import zipfile
 import sys
+import csv
 
 cc_files = {
     "extract_acct_submit": [
@@ -154,6 +155,69 @@ cc_files = {
       "trustee"
     ]
 }
+    
+def to_file(bcpdata, csvfilename="", col_headers=None):
+    if csvfilename=="":
+        csvfilename = 'converted.csv'
+        
+    # have to check system version annoyingly
+    if sys.version_info >= (3,0):
+    
+        # python3 csv writer needs strings
+        with open(csvfilename, 'w', encoding='utf-8') as csvfile:
+            if(col_headers):
+                for c in col_headers:
+                    c = c
+                writer = csv.writer(csvfile)
+                writer.writerow(col_headers)
+            csvfile.write(bcpdata)
+        
+    else:
+    
+        # python2 csv writer needs bytes
+        with open(csvfilename, 'wb') as csvfile:
+            if(col_headers):
+                for c in col_headers:
+                    c = c.encode('utf-8')
+                writer = csv.writer(csvfile)
+                writer.writerow(col_headers)
+            csvfile.write(bcpdata.encode('utf-8'))
+        
+    return csvfilename
+
+def import_zip_stream(zip_file):
+    zf = zipfile.ZipFile(zip_file, 'r')
+    print('Opened zip file: %s' % zip_file)
+    for filename in cc_files:
+        try:
+            bcp_filename = filename + '.bcp'
+            csv_filename = filename + '.csv'
+            col_headers=cc_files[filename]
+        
+            # have to check system version annoyingly
+            # for python 3 >
+            if sys.version_info >= (3,0):
+                
+                with zf.open(bcp_filename, 'r') as bcpfile:
+                    with open(csv_filename, 'w', newline='') as csvfile:
+                        writer = csv.writer(csvfile)
+                        writer.writerow(col_headers)
+                        for bcpfields in bcp.stream(bcpfile):
+                            writer.writerow(bcpfields)
+            
+            # for python 2
+            else:
+                
+                with zf.open(bcp_filename, 'r') as bcpfile:
+                    with open(csv_filename, 'wb') as csvfile:
+                        writer = csv.writer(csvfile)
+                        writer.writerow(col_headers)
+                        for bcpfields in bcp.stream(bcpfile):
+                            writer.writerow(bcpfields)
+                            
+            print('Converted: %s' % bcp_filename)
+        except KeyError:
+            print('ERROR: Did not find %s in zip file' % bcp_filename)
 
 def import_zip(zip_file):
     zf = zipfile.ZipFile(zip_file, 'r')
@@ -165,11 +229,11 @@ def import_zip(zip_file):
             bcpdata = zf.read(bcp_filename)
             bcpdata = bcpdata.decode('utf-8', errors="replace")
             bcpdata = bcp.convert(bcpdata)
-            bcp.to_file(bcpdata, csvfilename=csv_filename, col_headers=cc_files[filename])
+            to_file(bcpdata, csvfilename=csv_filename, col_headers=cc_files[filename])
             print('Converted: %s' % bcp_filename)
         except KeyError:
             print('ERROR: Did not find %s in zip file' % bcp_filename)
-
+            
 def main():
     zip_file = sys.argv[1]
     import_zip(zip_file)
